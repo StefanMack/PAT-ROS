c5990dad6b9f48c42305cf5e6a3eca60c8c79173
Stefan Mack <43177918+StefanMack@users.noreply.github.com>
2020-04-12 18:04:39 +0200

    USR§ LED indicating aktive WLAN connection

--- Utilities/wifi_reset.sh
+++ Utilities/wifi_reset.sh
@@ -2,7 +2,10 @@
 # Bash Skript, damit beim BBG nach dem Booten das WLAN aktiviert wird.
 # Funktioniert wohl nur, wenn als crontab nicht nur unter dem User beagle
 # sondern allgemein als root ausgeführt.
-# S. Mack, 14.12.19
+# S. Mack, 10.4.20
+
+# USR3-LED (sonst fuer eMMC) ausschalten
+echo none > /sys/class/leds/beaglebone\:green\:usr3/trigger
 
 # check if iwgetid command returns a wireless connection e.g. wlan0
 # command output is piped to Nirvana (>/dev/null)
@@ -11,7 +14,10 @@ iwgetid >/dev/null
 if [ $? != 0 ]
 then
     # Restart the wireless interface (disable necessary but causess error
-    # message that ist piped to Nirmana (2>/dev/null)
+    # message that ist piped to Nirvana (2>/dev/null)
     connmanctl disable wifi 2>/dev/null
     connmanctl enable wifi
+else
+   # USR3-LED (sonst fuer eMMC) blinken lassen
+   echo timer > /sys/class/leds/beaglebone\:green\:usr3/trigger
 fi
