ef29a7e6881875e40d469692574571aba6185196
Stefan Mack <43177918+StefanMack@users.noreply.github.com>
2020-05-28 08:57:31 +0200

    LSB MSB swap implemented, work now with µs output

--- PythonCodeBspl/mySRF02_smbus.py
+++ PythonCodeBspl/mySRF02_smbus.py
@@ -1,7 +1,7 @@
 ﻿#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # Abstandsmessung mit Ultraschallsensor SRF02
-# S. Mack, 25.11.19
+# S. Mack, 28.5.20
 
 import sys
 import smbus
@@ -17,9 +17,14 @@ print('====================================')
 
 try:
     while True:
-        i2c.write_byte_data(0x70, 0, 0x51)
+	# 0x51 Messwert in cm, 0x52 Messwert in µs
+        i2c.write_byte_data(0x70, 0, 0x52)
         time.sleep(0.2)
-        print(i2c.read_word_data(0x70, 2) / 255)
+        # 2 Byte lesen ab Adresse 0x02
+        wert = i2c.read_word_data(0x70, 0x02)
+        # Messwert Big Endian > MSB und LSB tauschen
+        wert = ((wert << 8) & 0xFF00) + (wert >> 8)
+        print(wert)
         time.sleep(1)
 except KeyboardInterrupt:
     print('')
