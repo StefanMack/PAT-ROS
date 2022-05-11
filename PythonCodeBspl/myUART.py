4f464463de5be27ead55b34c07c2863b4e4b0452
Stefan Mack <43177918+StefanMack@users.noreply.github.com>
2022-05-11 22:12:23 +0200

    Code alternatives for UART1 and UART4

--- PythonCodeBspl/myUART.py
+++ PythonCodeBspl/myUART.py
@@ -6,8 +6,13 @@
 # 0...1 mit gesendet. Da der Arduino nur ein ASCII-Zeichen als
 # Startsignal für den Messvorgang liest, gehen keine zweistelligen
 # Zahlen.
-# P9_21 ist TX, P9_22 ist RX am BB
-# S. Mack, 19.2.21
+# P9_21 ist TX, P9_22 ist RX am BB fuer UART2 (Grove Buchse auf BB-Board oberhalb SD-Karte
+# P9_24 ist TX, P9_26 ist RX am BB fuer UART1 (Grove Buchse auf Cape GPIO 15,14)
+# P9_11 ist RX, P9_13 ist TX am BB fuer UART4 (Grove Buchse auf Cape GPIO 30,31)
+# ACHTUNG: Falls Cape verwendet wird, richtigen Spannungspegel einstellen, auf 
+# BB-Board nur 3,3 V Pegel verwenden. 5 V Pegel funktioniert vermutlich nicht auf Cape
+
+# S. Mack, 11.5.22
 
 from subprocess import call
 import serial
@@ -16,9 +21,13 @@ from time import sleep
 
 print("Python-Interpreter: {}\n".format(sys.version))
 # Pinkonfiguration UART setzen
-command = "config-pin p9.21  uart"
+#command = "config-pin p9.21  uart" # Grove Buchse auf BB Bord ueber SD-Karte
+#command = "config-pin p9.24  uart" # Grove Buchse auf BB cape GPIO 15
+command = "config-pin p9.11  uart" # Grove Buchse auf BB cape GPIO 30
 call(command, shell= True) # execute command in Bash Shell
-command = "config-pin p9.22  uart"
+#command = "config-pin p9.22  uart" # Grove Buchse auf BB Bord ueber SD-Karte
+#command = "config-pin p9.26  uart" # Grove Buchse auf BB cape GPIO 14
+command = "config-pin p9.13  uart" # Grove Buchse auf BB cape GPIO 31
 call(command, shell= True)
 
 
@@ -30,7 +39,9 @@ print('...zwei Sekunden warten...')
 sleep(2)
 
 try:
-    port = serial.Serial(port='/dev/ttyO2', baudrate = 115200, timeout = 1)
+    #port = serial.Serial(port='/dev/ttyO2', baudrate = 115200, timeout = 1)
+    #port = serial.Serial(port='/dev/ttyO1', baudrate = 115200, timeout = 1)
+    port = serial.Serial(port='/dev/ttyO4', baudrate = 115200, timeout = 1)
     sleep(1) # nötig für eventuellen Reset Arduinoprogramm
     print('Arduino sagt: ', port.readline().decode("utf-8")) # Testen ob Arduino bereit
     sleep(0.5)
